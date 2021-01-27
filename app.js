b9576dec23658fb4962f5c7ba9861159420c2652
Jaime Rivera <riverac85@gmail.com>
2021-01-27 12:55:43 -0500

    ajustes de login y registro y transiciones

--- app.js
+++ app.js
@@ -8,9 +8,14 @@ app.listen(3030, ()=>{
     console.log("servidor corriendo");
 })
 
-
 app.get('/', (req,res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
 })
 
+app.get('/registro', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+})
 
+app.get('/login', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+})
